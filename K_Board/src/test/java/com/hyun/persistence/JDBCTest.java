8f4b9ee01d371dc22d6b50121116b9cf306999cb
Hyunwoo <hyunwoo2546@naver.com>
2022-04-03 20:50:02 +0900

    4/3

---
+++ K_Board/src/test/java/com/hyun/persistence/JDBCTest.java
@@ -0,0 +1,32 @@
+package com.hyun.persistence;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JDBCTest {
+
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testConnection() {
+		try (Connection con = DriverManager.getConnection(
+				"jdbc:oracle:thin:@localhost:1521:xe",
+				"K_DB",
+				"gus31216")){
+			System.out.println(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+	
+}
